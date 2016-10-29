a8e2313abcdb62e17ce2fb13e2d5eca20bc9c91d
LeeWongSnail <@wangli_0632@163.com>
2016-10-29 11:48:38 +0800

    init project

---
+++ ALiImageReshape/ALiImageReshape/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  ALiImageReshape
+//
+//  Created by LeeWong on 2016/10/29.
+//  Copyright © 2016年 LeeWong. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ ALiImageReshape/ALiImageReshape/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  ALiImageReshape
+//
+//  Created by LeeWong on 2016/10/29.
+//  Copyright © 2016年 LeeWong. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
