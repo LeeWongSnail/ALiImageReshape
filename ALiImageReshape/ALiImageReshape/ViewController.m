24da3910840326a5c1184e559c3d8c37959250bc
LeeWongSnail <@wangli_0632@163.com>
2016-10-29 12:12:12 +0800

    implement sys method

--- ALiImageReshape/ALiImageReshape/ViewController.m
+++ ALiImageReshape/ALiImageReshape/ViewController.m
@@ -8,11 +8,29 @@
 
 #import "ViewController.h"
 
-@interface ViewController ()
+@interface ViewController () <UIImagePickerControllerDelegate,UINavigationControllerDelegate>
 
+@property (weak, nonatomic) IBOutlet UIImageView *chooseImage;
 @end
 
 @implementation ViewController
+- (IBAction)sysMethod:(UIButton *)sender {
+    UIImagePickerController *picker = [[UIImagePickerController alloc] init];
+    picker.sourceType = UIImagePickerControllerSourceTypePhotoLibrary;
+    picker.delegate = self;
+    picker.allowsEditing = YES;
+    [self.navigationController presentViewController:picker animated:YES completion:nil];
+}
+
+- (IBAction)customMethod:(UIButton *)sender {
+    UIImagePickerController *picker = [[UIImagePickerController alloc] init];
+    picker.sourceType = UIImagePickerControllerSourceTypePhotoLibrary;
+    picker.delegate = self;
+    [self.navigationController presentViewController:picker animated:YES completion:nil];
+}
+
+
+
 
 - (void)viewDidLoad {
     [super viewDidLoad];
@@ -23,6 +41,20 @@
 - (void)didReceiveMemoryWarning {
     [super didReceiveMemoryWarning];
     // Dispose of any resources that can be recreated.
+}
+
+- (void)imagePickerControllerDidCancel:(UIImagePickerController *)picker
+{
+    [picker dismissViewControllerAnimated:YES completion:nil];
+}
+
+- (void)imagePickerController:(UIImagePickerController *)picker didFinishPickingMediaWithInfo:(NSDictionary<NSString *,id> *)info
+{
+    if (picker.allowsEditing) {
+        UIImage* image = [info objectForKey:@"UIImagePickerControllerEditedImage"];
+        [self.chooseImage setImage:image];
+    }
+    [picker dismissViewControllerAnimated:YES completion:nil];
 }
 
 
